060e56256f291a0a42a31fb222eeb0afd97131b1
João Victor <joaovictorneves28@hotmail.com.br>
2024-01-02 19:49:08 -0300

    Segundo desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia (numero: "5918258", modelo: "Modelo 1", imei: "1111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone IPhone:");
+Smartphone iphone = new Iphone (numero: "7328452", modelo: "Modelo 3", imei: "88888888", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
